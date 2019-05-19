e2bbcdf666f5cad0af8a0b1c59c0c6c6fdf2aa8a
NatanielPont <npontcastells@gmail.com>
2019-05-19 05:21:38 +0200

    sync

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,6 +7,7 @@ import Phaser from 'phaser'
 import tile from '../assets/tilesets/slopes32mud.png'
 import coin from '../assets/img/coin7.png'
 import Player from '../assets/img/player.png'
+import Spark from '../assets/particles/blue.png'
 import map from '../assets/tilemaps/map.json'
 var player
 var cursors
@@ -15,6 +16,9 @@ let coins
 let mapGame
 let coinScore = 0
 let text
+let emitter
+var particles
+
 // let coinScore = 0
 function createCoins () {
   CoinLayer.forEach(object => {
@@ -29,7 +33,16 @@ function createCoins () {
 function collectCoin (player, coin) {
   coin.destroy(coin.x, coin.y) // remove the tile/coin
   coinScore++ // increment the score
-  text.setText(`Coins: ${coinScore}x`) // set the text to show the current score
+  console.log(coins)
+  console.log(coins.children)
+  console.log(coins.children.entries.length)
+  // text.setText(`Coins: ${coinScore} - 16 _ Available : ${coins.children.entries.length}`) // set the text to show the current score
+  text.setText(`Coins: ${coinScore} - 16`) // set the text to show the current score
+  if (coins.children.entries.length === 0) {
+    this.scene.restart()
+    coinScore=0;
+  }
+
   return false
 }
 
@@ -66,6 +79,7 @@ function preload () {
   this.load.spritesheet('coin', coin, { frameWidth: 32, frameHeight: 32 })
   this.load.spritesheet('player', Player, { frameWidth: 28, frameHeight: 22 })
   this.load.tilemapTiledJSON('map', map)
+  this.load.image('spark', Spark)
 }
 function create () {
   console.log('Created')
@@ -106,28 +120,46 @@ function create () {
 
   cursors = this.input.keyboard.createCursorKeys()
 
-  var help = this.add.text(16, 16, 'Arrows to move. "up" to jump.', {
+  var help = this.add.text(16, 16, 'Arrows to move & jump. Collect all coins to get next level.', {
     fontSize: '18px',
     fill: '#ffffff'
   })
   help.setScrollFactor(0)
   // score
-  text = this.add.text(570, 70, `Coins: ${coinScore}x`, {
+  text = this.add.text(570, 70, `Coins: ${coinScore} - 16`, {
     fontSize: '20px',
     fill: '#ffffff'
   })
   text.setScrollFactor(0)
+  particles = this.add.particles('spark')
+
+  emitter = particles.createEmitter({
+    x: player.body.x + player.body.width / 2,
+    y: player.body.y + player.body.height,
+    angle: { min: 140, max: 180 },
+    speed: 200,
+    gravityY: 300,
+    scale: { start: 0.2, end: 0 },
+    lifespan: { min: 500, max: 1000 },
+    blendMode: 'ADD'
+  })
 }
 function update () {
+  // emitter.setY(player.body.y)
   player.anims.play('idle', true)
   if (cursors.left.isDown) {
     player.setAccelerationX(-200)
     player.setFrame(2)
+    emitter.setPosition(player.body.x + player.body.width, player.body.y + player.body.height)
   } else if (cursors.right.isDown) {
     player.setAccelerationX(200)
     player.setFrame(1)
+    emitter.setPosition(player.body.x, player.body.y + player.body.height)
+
+    // emitter.setPosition(player.body.x+player.body.width , player.body.y+player.body.height)
   } else {
     player.setAccelerationX(0)
+    emitter.setPosition(player.body.x + player.body.width / 2, player.body.y + player.body.height)
   }
   var onTheGround = player.body.onFloor()
 
@@ -136,9 +168,7 @@ function update () {
     this.jumping = false
   }
   if (cursors.up.isDown && onTheGround) {
-    console.log('hola salto 1')
     player.setVelocityY(-this.JUMP_SPEED)
-
     this.jumps--
   }
   if (cursors.up.isUp && this.jumps > 0) {
